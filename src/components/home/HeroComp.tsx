8a54e25df881c82780ef01c3100adba5bea907f8
IYSA <youngscientist.iysa@gmail.com>
2026-05-19 10:34:33 +0700

    feat: add HeroComp component and update project dependencies

--- src/components/home/HeroComp.tsx
+++ src/components/home/HeroComp.tsx
@@ -11,8 +11,9 @@ const HeroComp = () =>{
                 <p>Registration is now open for the International Youth Chemistry Olympiad! <br /> Join this prestigious event and gain an unforgettable experience!
                 </p>
                 <br />
-                <Link href="registration" className="registration-button">Registration Here!</Link>
-                <Link href="https://drive.google.com/file/d/1-eIYaEq6btaO41-XnlutwoGWxhnsgHeH/view?usp=sharing" target="_blank" className="registration-button">GuideBook</Link>
+                {/* <Link href="registration" className="registration-button">Registration Here!</Link> */}
+                <Link href="#" className="registration-button">Registration Closed!</Link>
+                {/* <Link href="https://drive.google.com/file/d/1-eIYaEq6btaO41-XnlutwoGWxhnsgHeH/view?usp=sharing" target="_blank" className="registration-button">GuideBook</Link> */}
             </div>
         </section>
         </>
