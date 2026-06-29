f2bd38e78a7c192b7a8fce1e63e92ff913f42b4e
IYSA <youngscientist.iysa@gmail.com>
2026-06-29 11:08:47 +0700

    UP DOKUM

--- src/components/NavbarComp.tsx
+++ src/components/NavbarComp.tsx
@@ -159,6 +159,13 @@ const NavbarComp = () =>{
                         </Link>
                         <ul className='sub-menu'>
                             <li className='sub-item'>
+                                <Link href="https://drive.google.com/drive/folders/1oOhwxz14ec6hBHwiMV3jL8njIsfJXS2L?usp=sharing" 
+                                    target='_blank'
+                                    rel="noopener noreferrer">
+                                        2026
+                                </Link>
+                            </li>
+                            <li className='sub-item'>
                                 <Link href="https://drive.google.com/drive/folders/1g_FTKMi1CdUKKFrvOyhysCSKpPtPHCOY?usp=sharing" 
                                     target='_blank'
                                     rel="noopener noreferrer">
